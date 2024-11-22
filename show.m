47ef7a2c3bfb8020bf90265b3a03c07be928aa18
Juliusz Pawlus <60894769+Bladdyy@users.noreply.github.com>
2024-11-22 12:00:04 +0100

    Update show.m

--- show.m
+++ show.m
@@ -3,6 +3,8 @@
 function show (N, x)
   size = N - 1;  % Size.
   x = reshape(x', size, size);  % Reshaping results to get mesh.
+  x = [zeros(size, 1), x, zeros(size, 1)];  % Adding zeros at the sides.
+  x = [zeros(1, size + 2); x; zeros(1, size + 2)];  % Adding zeros at the top and the bottom.
   mesh(x);  % Plot.
 
   % Naming.
